cca3f9d7168c8b5abfc1ea9faca4348102375ab5
JustineSmithies <justine@smithies.me.uk>
2022-02-06 17:08:09 +0000

    Updated Qtile config.py matches section

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -391,10 +391,10 @@ keys = [
 workspaces = [
     {"name": " ₁", "key": "1", "matches": [Match(wm_class='firefox')], "layout": "monadtall"},
     {"name": " ₂", "key": "2", "matches": [Match(wm_class='kitty'), Match(wm_class='ranger')], "layout": "monadtall"},
-    {"name": " ₃", "key": "3", "matches": [Match(wm_class='vim')], "layout": "monadtall"},
+    {"name": " ₃", "key": "3", "matches": [Match(wm_class='nvim')], "layout": "monadtall"},
     {"name": " ₄", "key": "4", "matches": [Match(wm_class='telegram-desktop'), Match(wm_class='weechat')], "layout": "monadtall"},
     {"name": " ₅", "key": "5", "matches": [Match(wm_class='gimp-2.99')], "layout": "monadtall"},
-    {"name": "阮 ₆", "key": "6", "matches": [Match(wm_class='spotify')], "layout": "monadtall"},
+    {"name": "阮 ₆", "key": "6", "matches": [Match(wm_class='Spotify')], "layout": "monadtall"},
     {"name": " ₇", "key": "7", "matches": [Match(wm_class='soffice')], "layout": "monadtall"},
     {"name": " ₈", "key": "8", "matches": [Match(wm_class='newsboat')], "layout": "monadtall"},
     {"name": " ₉", "key": "9", "matches": [Match(wm_class='neomutt')], "layout": "monadtall"},
